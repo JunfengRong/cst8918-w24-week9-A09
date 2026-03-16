ca2e24c11fbe969b6347136bbb3cd35756855320
Junfeng Rong <rongjunfeng09@gmail.com>
2026-03-16 14:00:23 -0400

    test tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,20 @@
+
+
+terraform {
+  required_version = ">= 1.0.0" 
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
+
 
 resource "azurerm_resource_group" "example" {
   name     = "storage-rg"
@@ -18,3 +32,4 @@ resource "azurerm_storage_account" "example" {
   tags = {
     environment = "staging"
   }
+}
